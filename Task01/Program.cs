75cf8e07816e67c30f00319175be4b7188928498
Vladimir_Chernyaev <>
2022-09-18 16:47:28 +0400

    first programm is ready

---
+++ Task01/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int[] array = new int[20];
+CreateArray(array);
+PrintArray(array);
+
+void CreateArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+
+}
+
+int FindEvenNum (int[] array)
+{
+    int count = 0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i]%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine();
+Console.WriteLine($"Колличество чётных элементов в массиве {FindEvenNum(array)}");
